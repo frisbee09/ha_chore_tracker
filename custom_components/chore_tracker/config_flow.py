4f980d88f2359d4c515c4dcc8122e5b6e2a60b88
Chris Johnstone <5574633+frisbee09@users.noreply.github.com>
2024-07-21 20:00:03 +0100

    init, structure from ha dev env

---
+++ custom_components/chore_tracker/config_flow.py
@@ -0,0 +1,52 @@
+"""Config flow for Chore Tracker integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+# TODO adjust the data schema to the data that you need
+STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required("chore_group_name"): str})
+
+
+class ChoreTrackerConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Chore Tracker."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        await self.async_set_unique_id(user_input["chore_group_name"])
+        self._abort_if_unique_id_configured()
+
+
+        return self.async_create_entry(title=user_input["chore_group_name"], data=user_input)
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle reconfiguration of the integration."""
+
+        return await self.async_step_user(user_input)
+
+
+
+
